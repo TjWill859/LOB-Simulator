b5ec197690c372553efca07da463af18f0d0529d
TjWill859 <toluwajuwilliams@gmail.com>
2026-09-17 17:23:04 -0700

    declared what an order book can do and defined how it does it

---
+++ include/order_book.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "types.hpp"
+
+class OrderBook {
+public:
+    void addOrder(const Order& order);
+};
+
---
+++ src/order_book.cpp
@@ -0,0 +1,5 @@
+#include "order_book.hpp"
+
+void OrderBook::addOrder(const Order& order) {
+    (void)order;
+}
